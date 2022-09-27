b55a5277cb66e5797f4bc1f7f26d454aa8189200
gabrieltiserra <djbieelzynn@gmail.com>
2022-09-26 21:28:09 -0300

    modified:   jogao.js meu mundo caiu

--- jogao.js
+++ jogao.js
@@ -1,7 +1,7 @@
     let xBolinha = 300;
     let yBolinha = 200;
     let diametro = 15;
-    let raiovallecano = diametro / 2;
+    let raio = diametro / 2;
 
     let velocidadeXBolinha = 6;
     let velocidadeYBolinha = 6;
